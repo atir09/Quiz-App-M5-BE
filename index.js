a08303be4702a3e8f0884847f44b98f9196ccc77
atir_09 <atirkhan0906@gmail.com>
2023-07-17 16:06:15 +0530

    Backend for Quiz-App

--- index.js
+++ index.js
@@ -41,5 +41,5 @@ app.listen(process.env.PORT,async()=>{
         console.log(error)
     }
 
-    console.log(`Listening on port ${process.env.port}`)
+    console.log(`Listening on port ${process.env.port }`)
 })
